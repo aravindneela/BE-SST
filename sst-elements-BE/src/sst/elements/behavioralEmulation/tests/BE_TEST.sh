51ed40e51706e72bdefe7baddad7b0a48e2c35a9
aravindneela <aravindneelakantan@gmail.com>
2021-03-04 15:56:16 -0500

    Updated BE_TEST script with extra test cases

--- sst-elements-BE/src/sst/elements/behavioralEmulation/tests/BE_TEST.sh
+++ sst-elements-BE/src/sst/elements/behavioralEmulation/tests/BE_TEST.sh
@@ -5,9 +5,9 @@ extractedSimTime=$(echo $string | sed -e 's/Simulation is complete, simulated ti
 
 if [[ $extractedSimTime = "22.4" ]]
 then
-	echo "Computation test case 1 ---- PASS _/"
+	echo "Computation test case 1   ----  PASS _/"
 else
-	echo "Computation test case 1 ---- FAIL X"
+	echo "Computation test case 1   ----  FAIL X"
 fi
 
 
@@ -17,42 +17,42 @@ extractedSimTime=$(echo $string | sed -e 's/Simulation is complete, simulated ti
 
 if [[ $extractedSimTime = "22.4" ]]
 then
-	echo "Computation test case 2 ---- PASS _/"
+	echo "Computation test case 2   ----  PASS _/"
 else
-	echo "Computation test case 2 ---- FAIL X"
+	echo "Computation test case 2   ----  FAIL X"
 fi
 
 ./BE_RUN.sh -c comm_test1.py > test_output.txt
 string=$(grep "simulated time:" test_output.txt)	#echo "${string#*:}"
 extractedSimTime=$(echo $string | sed -e 's/Simulation is complete, simulated time: \(.*\) s/\1/')
 
-if [[ $extractedSimTime = "205.1" ]]
+if [[ $extractedSimTime = "204.8" ]]
 then
-	echo "Communication test case 1 ---- PASS _/"
+	echo "Communication test case 1 ----  PASS _/"
 else
-	echo "Communication test case 1 ---- FAIL X"
+	echo "Communication test case 1 ----  FAIL X"
 fi
 
 ./BE_RUN.sh -c comm_test2.py > test_output.txt
 string=$(grep "simulated time:" test_output.txt)	#echo "${string#*:}"
 extractedSimTime=$(echo $string | sed -e 's/Simulation is complete, simulated time: \(.*\) s/\1/')
 
-if [[ $extractedSimTime = "153.6" ]]
+if [[ $extractedSimTime = "1.2" ]]
 then
-	echo "Communication test case 2 ---- PASS _/"
+	echo "Communication test case 2 ----  PASS _/"
 else
-	echo "Communication test case 2 ---- FAIL X"
+	echo "Communication test case 2 ----  FAIL X"
 fi
 
 ./BE_RUN.sh -c comm_test3.py > test_output.txt
 string=$(grep "simulated time:" test_output.txt)	#echo "${string#*:}"
 extractedSimTime=$(echo $string | sed -e 's/Simulation is complete, simulated time: \(.*\) s/\1/')
 
-if [[ $extractedSimTime = "102.4" ]]
+if [[ $extractedSimTime = "6.4" ]]
 then
-	echo "Communication test case 3 ---- PASS _/"
+	echo "Communication test case 3 ----  PASS _/"
 else
-	echo "Communication test case 3 ---- FAIL X"
+	echo "Communication test case 3 ----  FAIL X"
 fi
 
 ./BE_RUN.sh -c comm_testx.py > test_output.txt
@@ -61,9 +61,19 @@ extractedSimTime=$(echo $string | sed -e 's/Simulation is complete, simulated ti
 
 if [[ $extractedSimTime = "28.8" ]]
 then
-	echo "Communication test case x ---- PASS _/"
+	echo "Comm & Comp test case 1   ----  PASS _/"
 else
-	echo "Communication test case x ---- FAIL X"
+	echo "Comm & Comp test case 1   ----  FAIL X"
 fi
 
+./BE_RUN.sh -c comm_testx2.py > test_output.txt
+string=$(grep "simulated time:" test_output.txt)	#echo "${string#*:}"
+extractedSimTime=$(echo $string | sed -e 's/Simulation is complete, simulated time: \(.*\) s/\1/')
+
+if [[ $extractedSimTime = "25.6" ]]
+then
+	echo "Comm & Comp test case 2   ----  PASS _/"
+else
+	echo "Comm & Comp test case 2   ----  FAIL X"
+fi
 
